c9dc1bcea73f3e4306f04488457d602cbf3d4bbd
Yadhukrishnan P <yadhukrishnannair99@gmail.com>
2024-10-16 23:19:28 -0700

    mild update

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [answer, setAnswer] = useState();
+	const [guess, setGuess] = useState();
+	const [stage, setStage] = useState(0);
 
 	return (
 		<>
 			<h1>Hangman</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<div>
+			<StagePicture stage={stage} />
 		</>
 	)
 }
